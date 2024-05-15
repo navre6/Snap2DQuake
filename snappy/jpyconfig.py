ca43b737dbcd4e394d89ac22aaf231fb4579c18e
Martina Occhipinti <134698198+navre6@users.noreply.github.com>
2024-05-15 16:07:52 +0200

    Deleted snappy directory

--- snappy/jpyconfig.py
+++
@@ -1,8 +0,0 @@
-# Created by 'jpyutil.py' tool on 2023-05-09 17:47:37.058713
-# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
-java_home = 'C:\\Program Files\\snap\\jre\\jre'
-jvm_dll = 'C:\\Program Files\\snap\\jre\\jre\\bin\\server\\jvm.dll'
-jvm_maxmem = None
-jvm_classpath = []
-jvm_properties = {}
-jvm_options = []
